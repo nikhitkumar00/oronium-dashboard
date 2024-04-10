5fbbc459d505957c7ec6fe5cad014b0a2a910631
Nikhit kumar <nikhitkumar00@gmail.com>
2024-04-10 09:12:08 +0530

    Added Header

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import { BellIcon, PlayIcon } from "@/public/svg/icons";
+import Image from "next/image";
+const Header = () => {
+  return (
+    <div className="w-full px-6 py-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Image
+            className="size-10"
+            src="/logo.png"
+            alt="logo"
+            width={100}
+            height={100}
+          />
+          <h1 className="text-textDark font-semibold">
+            Oronium E-Commerce Store
+          </h1>
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <button className="flex cursor-pointer items-center justify-center gap-2 rounded bg-red-600 px-3 py-3 text-xs font-semibold text-white hover:bg-red-500">
+            <PlayIcon className="size-4" />
+            Preview Store
+          </button>
+          <button className="cursor-pointer rounded bg-gray-200 p-2 hover:bg-gray-300">
+            <BellIcon className="size-5 stroke-2" />
+          </button>
+          <Image
+            src="/avatar.png"
+            alt="avatar"
+            width={40}
+            height={40}
+            className="size-9 cursor-pointer rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
---
+++ public/svg/icons.tsx
@@ -0,0 +1,32 @@
+const PlayIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        stroke="currentColor"
+        d="M6.906 4.537A.6.6 0 0 0 6 5.053v13.894a.6.6 0 0 0 .906.516l11.723-6.947a.6.6 0 0 0 0-1.032z"
+      />
+    </svg>
+  );
+};
+
+const BellIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3H4a4 4 0 0 0 2-3v-3a7 7 0 0 1 4-6M9 17v1a3 3 0 0 0 6 0v-1"
+      />
+    </svg>
+  );
+};
+export { PlayIcon, BellIcon };
